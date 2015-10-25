cf09e45ec038558ba99003d1abcb27fbd4dde3fa
Michael Odintsov <michael@datarobot.com>
2015-10-25 12:47:11 +0200

    improved amortized cost

--- playground.py
+++ playground.py
@@ -21,10 +21,10 @@ for card_set in card_sets_available_in_store:
             stats[card_set][rarity][0] + stats[card_set][rarity][1] + stats[card_set][rarity][2])) ** 5
         )
         if rarity != 'Legendary':
-            amortized_booster_dust_cost += dust_cost_to_craft[rarity] * (1 - (1 - probabilities_in_booster[rarity] * (stats[card_set][rarity][0] + stats[card_set][rarity][1]) / (
-                stats[card_set][rarity][0] + stats[card_set][rarity][1] + stats[card_set][rarity][2])) ** 5)
+            amortized_booster_dust_cost += 5 * dust_cost_to_craft[rarity] * probabilities_in_booster[rarity] * (stats[card_set][rarity][0] + stats[card_set][rarity][1]) / (
+                stats[card_set][rarity][0] + stats[card_set][rarity][1] + stats[card_set][rarity][2])
         else:
             # we don't need 2nd copy of legendaries, as we only can use 1
-            amortized_booster_dust_cost += dust_cost_to_craft[rarity] * (1 - (1 - probabilities_in_booster[rarity] * (stats[card_set][rarity][0]) / (
-                stats[card_set][rarity][0] + stats[card_set][rarity][1] + stats[card_set][rarity][2])) ** 5)
+            amortized_booster_dust_cost += 5 * dust_cost_to_craft[rarity] * probabilities_in_booster[rarity] * (stats[card_set][rarity][0]) / (
+                stats[card_set][rarity][0] + stats[card_set][rarity][1] + stats[card_set][rarity][2])
     print 'Amortized dust cost of new cards and second copies in booster %f' % amortized_booster_dust_cost
