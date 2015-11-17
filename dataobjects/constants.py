4bb4f67121b0cc6f319d1bab7909452b412621cf
Michael Odintsov <michael@datarobot.com>
2015-11-17 16:40:13 +0200

    The League of Explorers cards

--- dataobjects/constants.py
+++ dataobjects/constants.py
@@ -3,7 +3,8 @@ from api_tools.tools import get_all_legendaries, get_cards_for_all_classes, get_
 card_types = ['Hero', 'Enchantment', 'Hero Power', 'Weapon', 'Spell', 'Minion']
 card_types_in_collection = ['Weapon', 'Spell', 'Minion']
 card_sets = ['Hero Skins', 'Missions', 'Classic', 'Naxxramas', 'Tavern Brawl', 'System', 'Credits',
-             'Blackrock Mountain', 'Basic', 'Debug', 'Promotion', 'Reward', 'The Grand Tournament', 'Goblins vs Gnomes']
+             'Blackrock Mountain', 'Basic', 'Debug', 'Promotion', 'Reward', 'The Grand Tournament', 'Goblins vs Gnomes',
+             'The League of Explorers']
 card_sets_available_in_store = ['Classic', 'Goblins vs Gnomes', 'The Grand Tournament']
 card_rarities = ['Legendary', 'Epic', 'Rare', 'Common', 'Free']
 probabilities_in_booster = {'Legendary': 0.01, 'Epic': 0.04, 'Rare': 0.2, 'Common': 0.75}
